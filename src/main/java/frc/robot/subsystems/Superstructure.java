3f9f2ba63e5a852c3b8fc0df67b7eb0d2231215b
Aidan <akcopinga@gmail.com>
2019-01-17 09:01:19 -0700

    initial import

---
+++ src/main/java/frc/robot/subsystems/Superstructure.java
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+
+/**
+ * The superstructure subsystem is the overarching superclass containing all components of the superstructure: the
+ * intake, hopper, feeder, shooter and LEDs. The superstructure subsystem also contains some miscellaneous hardware that
+ * is located in the superstructure but isn't part of any other subsystems like the compressor, pressure sensor, and
+ * hopper wall pistons.
+ * <p>
+ * Instead of interacting with subsystems like the feeder and intake directly, the {@link Robot} class interacts with
+ * the superstructure, which passes on the commands to the correct subsystem.
+ * <p>
+ * The superstructure also coordinates actions between different subsystems like the feeder and shooter.
+ *
+ * @see Subsystem
+ */
+public class Superstructure extends Subsystem {
+    // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+}
