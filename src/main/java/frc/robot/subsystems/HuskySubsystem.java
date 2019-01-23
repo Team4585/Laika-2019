c593aaef085ced3412bdedf3ecbc7dfb9a29a295
Isaiah Stotesbery <sirstotes@gmail.com>
2019-01-23 15:36:36 -0700

    Create HuskySubsystem.java

---
+++ src/main/java/frc/robot/subsystems/HuskySubsystem.java
@@ -0,0 +1,13 @@
+package frc.team4585.robot;
+
+public interface HuskySubsystem {
+	
+	public void autoInit();
+	
+	public void doAuto();
+	
+	public void teleopInit();
+	
+	public void doTeleop();
+	
+}
