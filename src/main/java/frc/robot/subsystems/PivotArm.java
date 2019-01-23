2d8dc3007bf71349cd6b9ad04d7af9d6eca38cfa
Isaiah Stotesbery <sirstotes@gmail.com>
2019-01-23 15:49:04 -0700

    Create PivotArm.java

---
+++ src/main/java/frc/robot/subsystems/PivotArm.java
@@ -0,0 +1,28 @@
+
+package frc.robot.subsystems;
+
+public class PivotArm extends HuskySubsystem
+{
+      private double TargetAngle;
+
+      public PivotArm (int MotorPort, int SensorPort)
+      {
+             //set motor
+             //set sensor
+      }
+  
+      public void SetTarget (double angle)
+      {
+            TargetAngle = angle;
+      }
+  
+      public void onDeactivate ()
+      {
+            SetTarget();
+      }
+      
+      public void doTeleop ()
+      {
+            //move to target position
+      }
+}
