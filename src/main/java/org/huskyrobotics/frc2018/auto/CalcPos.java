83e35ff05c76e13c0c68b06765af243c736e7f4a
Dhhoyt <36286620+Dhhoyt@users.noreply.github.com>
2019-02-05 08:54:34 -0700

    Update CalcPos.java

--- src/main/java/org/huskyrobotics/frc2018/auto/CalcPos.java
+++ src/main/java/org/huskyrobotics/frc2018/auto/CalcPos.java
@@ -1,3 +1,14 @@
-class CalcPos{
+import edu.wpi.first.wpilibj.Encoder;
 
+class CalcPos{
+  double[] getPos(){
+    
+  }
+  void addData(double step){
+  
+  }
+  void initPos(){
+    Encoder lEncoder = new Encoder(5, 5);
+    Encoder lEncoder = new Encoder(6, 6);
+  }
 }
