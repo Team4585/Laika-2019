d311a8d440e365494e51125e80b4f10d2984097d
Isaiah Stotesbery <sirstotes@gmail.com>
2019-02-13 15:01:56 -0700

    Fix

--- src/main/java/org/huskyrobotics/frc2019/subsystems/superstructure/PivotArm.java
+++ src/main/java/org/huskyrobotics/frc2019/subsystems/superstructure/PivotArm.java
@@ -1,26 +1,25 @@
-
 package org.huskyrobotics.frc2019.subsystems.superstructure;
 
 //import org.huskyrobotics.frc2019.subsystems.*;
-import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.*;
+//import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class PivotArm extends Subsystem {
       private double m_targetAngle;
       private double m_currentAngle;
 
-      private VictorSPX m_motor;
-      private AnalogPotentiometer m_potent;
-
-      private final int c_P = 1;
-      private final int c_I = 1;
-      private final int c_D = 1;
-      private int integral = 0;
-      private double previousError, error = 0;
-      private final double c_timeConstant = 0.02;
-      private double PIDOutput = 0;
+      private TalonSRX m_motor;
+      //private AnalogPotentiometer m_potent;
+      //variables used for PID
+      private final double kP = 1;//Speed Constant
+      private final double kI = 0.001;//Speed up constant
+      private final double kD = 1;//Slow down constant
+      private final int kTimeoutMs = 100;
+      private final int kF = 1;
 
       public void initDefaultCommand() 
 	{
@@ -30,8 +29,39 @@ public class PivotArm extends Subsystem {
 	}
 
       public PivotArm(int motorPort, int sensorPort) {
-		m_motor = new VictorSPX(motorPort);
-            m_potent = new AnalogPotentiometer(sensorPort, 360, 0);
+            m_motor = new TalonSRX(motorPort);
+            m_motor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 100);
+            m_motor.setSensorPhase(true);
+            m_motor.configNominalOutputForward(0, kTimeoutMs);
+            m_motor.configNominalOutputReverse(0, kTimeoutMs);
+            m_motor.configPeakOutputForward(1, kTimeoutMs);
+            m_motor.configPeakOutputReverse(-1, kTimeoutMs);
+            
+            m_motor.selectProfileSlot(0, 0);
+            m_motor.config_kF(0, kF, kTimeoutMs);
+            m_motor.config_kP(0, kP, kTimeoutMs);
+            m_motor.config_kI(0, kI, kTimeoutMs);
+            m_motor.config_kD(0, kD, kTimeoutMs);
+            m_motor.config_IntegralZone(0, 100, kTimeoutMs);
+      }
+      public void setArmAxis(double input) {
+            if(input > 0.1) {
+                  goUp();
+            } else if(input < -0.1) {
+                  goDown();
+            } else {
+                  stop();
+            }
+      }
+	public void setIsClimbActive (boolean input) {
+		if (input) {
+			setTarget(0);
+		}
+	}
+      //To be called by Robot.java. Will move the arm towards the target position.
+      public void periodic() {
+            calculateAngle();
+            m_motor.set(ControlMode.Position, m_targetAngle);
       }
 
       public double getCurrentAngle() {
@@ -50,28 +80,15 @@ public class PivotArm extends Subsystem {
       }
       public void stop() {
             setTarget(m_currentAngle);
-            resetPID();
+            resetEncoder();
       }
-
-      public void periodic() {
-            calculateAngle();
-            calculatePID();
-            
-            m_motor.set(ControlMode.PercentOutput, PIDOutput);
-      }
+      //Used to calculate the current angle of the arm
       private void calculateAngle() {
-            m_currentAngle = m_potent.get();
+            m_currentAngle = m_motor.getSelectedSensorPosition()/360;
       }
-      private void calculatePID() {
-            previousError = error;
-            error = m_targetAngle - m_currentAngle;
-            integral += (error*c_timeConstant);
-            var derivative = (error - previousError) / c_timeConstant;
-            PIDOutput = c_P*error + c_I*integral + c_D*derivative;
-      }
-      public void resetPID() {
-            integral = 0;
-            previousError = 0;
-            error = 0;
+      //Used when target is reached or when it should stop for any reason
+      public void resetEncoder() {
+            m_motor.setSelectedSensorPosition(0, 0, kTimeoutMs);
       }
 }
+ 
