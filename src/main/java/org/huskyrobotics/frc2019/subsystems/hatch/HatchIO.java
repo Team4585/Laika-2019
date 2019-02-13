911dee6acdd817e0821586e37511b708d47ca71e
Isaiah Stotesbery <sirstotes@gmail.com>
2019-02-13 15:02:32 -0700

    Update HatchIO.java

--- src/main/java/org/huskyrobotics/frc2019/subsystems/hatch/HatchIO.java
+++ src/main/java/org/huskyrobotics/frc2019/subsystems/hatch/HatchIO.java
@@ -3,12 +3,20 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 
 public class HatchIO {
-	private VictorSPX[] m_linearActuators;
+    private VictorSPX[] m_linearActuators;
     private double m_outSpeed = 1.0;
     private double m_inSpeed = 1.0;
 
     public HatchIO(int[] ActuatorPorts) {
         m_linearActuators = new VictorSPX[] {new VictorSPX(ActuatorPorts[0]), new VictorSPX(ActuatorPorts[1]), new VictorSPX(ActuatorPorts[2]), new VictorSPX(ActuatorPorts[3])};
+    }
+
+    public void setHatchPush (boolean input) {
+        if(input) {
+            release();
+        } else {
+            reset();
+        }
     }
 
     public void release () {
